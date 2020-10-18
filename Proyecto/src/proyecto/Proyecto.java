9bfc5a80710d4f6e51dff6e466ee69e434d7a003
Lukas <lukas.pinto@alumnos.uv.cl>
2020-10-17 23:00:41 -0300

    cambio de print

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -15,7 +15,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola angel araya asdf as  ");
+        System.out.println("Hola angel como estas llanoma ");
     }
     
 }
