c46f28364bd57f952c87f58db53a711c8d829e54
Benjamin <Benjamin@PC-Benjamin>
2020-10-17 23:40:15 -0300

    mas weas

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -15,7 +15,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        System.out.println("Bienvenido a 'Estacionamiento Ángel Araya' ");
     }
     
 }
