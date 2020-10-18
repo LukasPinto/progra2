8570ea4d6f50b4a06c274dea6392fab9d71ff985
Benjamin <Benjamin@PC-Benjamin>
2020-10-17 23:33:42 -0300

    gets y sets en estacionamiento

--- Proyecto/src/proyecto/Estacionamiento.java
+++ Proyecto/src/proyecto/Estacionamiento.java
@@ -5,5 +5,39 @@ public class Estacionamiento {
     
     private int numEstacionamiento;
     private String seccion;
+    private Vehiculo vehiculo;
+
+    // getters
+    public int getNumEstacionamiento() {
+        return numEstacionamiento;
+    }
+
+    public String getSeccion() {
+        return seccion;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    // setters
+    public void setNumEstacionamiento(int numEstacionamiento) {
+        this.numEstacionamiento = numEstacionamiento;
+    }
+
+    public void setSeccion(String seccion) {
+        this.seccion = seccion;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+    
+    
+    
+    public Estacionamiento(){
+        
+    }
+    
     
 }
