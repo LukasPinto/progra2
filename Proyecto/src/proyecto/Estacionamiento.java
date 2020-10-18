85590a1350d104a3da494dfa3931d64d1a19ce1b
Benjamin <Benjamin@PC-Benjamin>
2020-10-17 23:20:38 -0300

    puras weas

--- Proyecto/src/proyecto/Estacionamiento.java
+++ Proyecto/src/proyecto/Estacionamiento.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto;
 
-/**
- *
- * @author Raigou
- */
 public class Estacionamiento {
+    
+    private int numEstacionamiento;
+    private String seccion;
     
 }
