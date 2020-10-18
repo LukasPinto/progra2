958b1035ec6e09a501b40cc27375c7b642722c55
Raigou <Raigou@192.168.1.36>
2020-10-17 23:37:29 -0300

    borre los prints bastardos llanoma buen cote primo

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -15,8 +15,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola angel como estas llanoma ");
-        System.out.println("no puedo subir el archivo aaaaaaaaaaaaaaaaaaa");
+
     }
     
 }
