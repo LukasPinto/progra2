7efdcf15aa59353d6a48e357005a57e798018814
Raigou <Raigou@192.168.1.36>
2020-10-17 23:32:52 -0300

    agregue getters and setters y constructores a persona

--- Proyecto/src/proyecto/Persona.java
+++ Proyecto/src/proyecto/Persona.java
@@ -13,6 +13,39 @@ public class Persona {
     //atributos
     private String nombre;
     private String codigo;
-    
-    
+    private Vehiculo vehiculo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public Persona(String nombre, String codigo, Vehiculo vehiculo) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.vehiculo = vehiculo;
+    }
+
+    public Persona() {
+        
+    }  
 }
