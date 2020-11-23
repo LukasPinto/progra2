b53928d44aad298ad44f1ba4c104d7830814c792
Raigou <Raigou@192.168.1.36>
2020-11-23 20:30:10 -0300

    edite un poco archivo estacionamiento

--- Proyecto/src/proyecto/ArchivoEstacionamiento.java
+++ Proyecto/src/proyecto/ArchivoEstacionamiento.java
@@ -3,32 +3,12 @@ package proyecto;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 /**
  *
  * @author Angelaraya
  */
 public class ArchivoEstacionamiento{
 
-    public static void main(String[] escribir){
-        String texto = "";
-        try{
-            BufferedReader br;
-            FileReader fr = new FileReader("archivo.txt");
-            br = new BufferedReader(fr);
-            System.out.println("Contenido del txt: ");
-            String linea=br.readLine();
-            while (linea !=null){
-                System.out.println(linea);
-                linea=br.readLine();
-            }
-            br.close();          
-        }catch (IOException ioe){
-            System.out.println("Error al abrir el archivo");
-            ioe.printStackTrace();
-        }catch (Exception e){
-            System.out.println("Error al leer el teclado");
-            e.printStackTrace();
-        }
-    }
+
 }
+        
