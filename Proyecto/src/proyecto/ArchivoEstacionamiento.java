2079ed97671c55ca501f70033a7b29bd1320e192
Raigou <Raigou@192.168.1.36>
2020-11-23 20:01:55 -0300

    edite un poco archivo estacionamiento

--- Proyecto/src/proyecto/ArchivoEstacionamiento.java
+++ Proyecto/src/proyecto/ArchivoEstacionamiento.java
@@ -1,34 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
- * @author Benjamin
+ * @author Angelaraya
  */
 public class ArchivoEstacionamiento{
 
-    FileWriter escribir;
-    PrintWriter imprimir;
-
-    File archivo = new File("Estacionamiento.txt");
-
-
-
+    public static void main(String[] escribir){
+        String texto = "";
+        try{
+            BufferedReader br;
+            FileReader fr = new FileReader("archivo.txt");
+            br = new BufferedReader(fr);
+            System.out.println("Contenido del txt: ");
+            String linea=br.readLine();
+            while (linea !=null){
+                System.out.println(linea);
+                linea=br.readLine();
+            }
+            br.close();          
+        }catch (IOException ioe){
+            System.out.println("Error al abrir el archivo");
+            ioe.printStackTrace();
+        }catch (Exception e){
+            System.out.println("Error al leer el teclado");
+            e.printStackTrace();
+        }
+    }
 }
