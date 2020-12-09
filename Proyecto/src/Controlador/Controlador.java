961cd2e43009a73615ba11c60691df293b402079
Benjamin <Benjamin@PC-Benjamin>
2020-12-09 20:14:49 -0300

    error klo

--- Proyecto/src/Controlador/Controlador.java
+++ Proyecto/src/Controlador/Controlador.java
@@ -5,6 +5,14 @@
  */
 package Controlador;
 
+import Modelo.Vehiculo;
+import Modelo.Auto;
+import Modelo.Moto;
+import Modelo.Camion;
+import Modelo.Camioneta;
+import Modelo.RegistroEstacionamiento;
+import Modelo.ArchivoEstacionamiento;
+import Modelo.Caja;
 import java.util.ArrayList;
 import java.util.List;
 /**
@@ -12,5 +20,25 @@ import java.util.List;
  * @author Benjamin
  */
 public class Controlador {
+    private static List<Vehiculo> lista;
+
+    public Controlador() {
+        lista = new ArrayList<>();
+    }
     
+    public static boolean agregar(Vehiculo vehiculo) {
+        return lista.add(vehiculo);
+    }
+    public static List<Vehiculo> getLista() {
+        return lista;
+    }
+    
+    public static boolean eliminar(int patente) {
+        for (Vehiculo vehiculo : lista) {
+            if (vehiculo.getPatente() == patente) {
+                return lista.remove(vehiculo);
+            }
+        }
+        return false;
+    }
 }
